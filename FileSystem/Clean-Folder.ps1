811bab17c29901d222bb60d41402c4a443b9e1ea
Maxim Korsukov <mkorsukov@gmail.com>
2017-10-24 20:29:33 -0500

    Added the folder cleanup script

---
+++ FileSystem/Clean-Folder.ps1
@@ -0,0 +1,28 @@
+﻿# Folder Remover
+# Allows to cleanup the specified directory
+
+param
+(
+    [string] $folderPath
+)
+
+Clear-Host
+Write-Host "Folder Remover 1.0 : Copyright (C) Maxim Korsukov : 2017-08-01"
+
+if (!$folderPath)
+{
+    Write-Host "Required folder path is not specified" -ForegroundColor Red
+    Exit 1
+}
+
+if (!(Test-Path $folderPath))
+{
+    Write-Host "Required folder doesn't exist" -ForegroundColor Red
+    Exit 1
+}
+
+Write-Host "Cleaning folder '$folderPath'..."
+Get-ChildItem -Path "$folderPath" -include @('bin', 'obj', 'packages') -Directory -Recurse | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+
+Write-Host "OK" -ForegroundColor Green
+Exit 0
---
+++ FileSystem/readme.md
@@ -0,0 +1,14 @@
+# File System
+
+A set of test / validation / verification related scripts
+
+## Clean-Folder.ps1
+
+Allows to clean up the specified folder out of non-important content (`bin`, `obj`, `packages`)
+
+Script usage example:
+
+```powershell
+Clean-Folder.ps1
+    -folderPath "c:\project"
+```
